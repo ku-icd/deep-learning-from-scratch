cf5974d1b572d459cb107ba8a3c6e22e2d0d4b08
ku-icd <85928448+ku-icd@users.noreply.github.com>
2022-12-16 03:41:35 +0900

    first commit

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -10,14 +10,17 @@ def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
-img = x_train[0]
-label = t_train[0]
-print(label)  # 5
 
-print(img.shape)  # (784,)
-img = img.reshape(28, 28)  # 形状を元の画像サイズに変形
-print(img.shape)  # (28, 28)
+if __name__ == '__main__':
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
-img_show(img)
+    img = x_train[0]
+    label = t_train[0]
+    print(label)  # 5
+
+    print(img.shape)  # (784,)
+    img = img.reshape(28, 28)  # 形状を元の画像サイズに変形
+    print(img.shape)  # (28, 28)
+
+    img_show(img)
